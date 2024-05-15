5d36800a846611b45e7571f22047cbf2fd5b4e4b
monolab825 <p.smiles777@gmail.com>
2024-05-15 19:57:16 +0400

    updated

--- getBlockhash.py
+++ getBlockhash.py
@@ -154,7 +154,7 @@ def loadBlockData(substrate):
         df = pd.read_csv('blockHash.csv')
         start = int(df.iloc[-1].iloc[0])+1
 
-    for i in tqdm(range (start,20)):
+    for i in tqdm(range (start,70000)):
         blockHash = substrate.get_block_hash(i)
         era = (substrate.query(
         "Staking", "ActiveEra",block_hash=blockHash
